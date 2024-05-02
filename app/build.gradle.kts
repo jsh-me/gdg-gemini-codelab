a6f6cc198713aabbe06dd3e9de7bb1eed7c7d8e1
jsh-me <ppm_it@naver.com>
2024-05-02 23:51:17 +0900

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation("com.google.ai.client.generativeai:generativeai:0.3.0")
-    implementation("io.coil-kt:coil-compose:2.5.0")
+   // TODO implement generativeai
 }
